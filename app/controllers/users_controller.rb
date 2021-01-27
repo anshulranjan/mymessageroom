9b0b6d66d1f092bdb5074ab08b5a0c8ca8754f19
Ubuntu <ubuntu@ip-172-31-31-30.us-east-2.compute.internal>
2021-01-27 07:05:31 +0000

    added gender column in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,7 +63,7 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password , :gender)
   end
   
   def require_same_user
